418597d678662f8d9a23e0f99d2285494d25505c
bauyrzhan <103762895+bauyrzhan2050@users.noreply.github.com>
2022-08-18 18:30:24 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,140 @@
+//переменные-variables
+//
+// let
+// var
+// const
+
+
+// const userName = "Bauyrzhan";
+// console.log(userName);
+
+// const user = {
+//     name: "Baur",
+//     age:30,
+//     job: "IT"
+// }
+// user.age=22
+// console.log(user)
+
+// let userName = "Baur", age=38
+// age = 32
+// userName="Bauyrzhan"
+// console.log(userName, age)
+
+// типы данных
+// String   "", ``, ''
+// Number 
+// Boolean
+// Bigint
+// Symbol
+// object
+// function
+// underfined - 
+// null
+// infinity
+
+
+// let a = +prompt()
+// let b = +prompt()
+// console.log((a+b)*2)
+// console.log(a*b)
+// console.log(4*a)
+// console.log(a*b/2)
+
+
+// let age=5;
+// age++
+// console.log(age)
+// age*=5
+// console.log(age)
+// age/=5
+// console.log(age)
+// age-=2
+// console.log(age)
+
+// || или
+// let result= (true || false);
+// console.log(result);
+
+// let numbers = (10>20 || 10<20);
+// console.log(result);
+
+// && - и
+// let result= (true && false);
+// console.log(result);
+
+// let numbers = (10>20 || 10===10);
+// console.log(result);
+
+// let numbers = (10<20 || 10===10);
+// console.log(result);
+
+
+
+
+// !- не
+// let result= (!true === false && 10===10 || 30>10 && 10===30);
+// console.log(result);
+// let text = "a">"c"
+// console.log(text)
+
+
+// if(100>200){
+//     console.log("100 less then 200")
+// }else if (100>150){
+//     console.log("100 less then 150")
+// }else if(100===100){
+//     console.log("100 equal 100")
+// }else{
+//     console.log("does not work")
+// }
+
+// 1)Напишите программу, которая будет просить
+// пользователя вводить два числа, после чего,
+// высчитывать Сумму чисел(плюс),
+// произведение(умножение), разность(минус) и
+// частное(деление).
+
+// let a = +prompt()
+// let b = +prompt()
+// console.log(a+b)
+// console.log(a*b)
+// console.log(a/b)
+
+// 2)
+// let a = +prompt()
+// console.log(a-110)
+
+// //3)
+// let a = +prompt()
+// result1 =a*a
+// console.log(result1)
+// result=a**3
+// console.log(result)
+// 4)
+// let a = +prompt()
+
+// if(a>=100){
+// console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+// // 5)
+// let a = +prompt()
+// let b = +prompt()
+// if(a>b ){
+//     console.log("a bigger than b" )
+// }else if(a<b ){
+//     console.log("b bigger tnan a")
+// }else{
+//     console.log("b and a are aquals")
+// }
+
+// 6
+// let a = +prompt()
+// if (a>=0){
+//     console.log(" a possitive")
+// }else{
+//     console.log("a negative")
+// }
